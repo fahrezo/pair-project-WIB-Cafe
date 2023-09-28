5488b89c16171c95ba34f933e7f6b46dda47d41f
Fahreza Malik <fahrezamlk@gmail.com>
2023-09-28 17:23:39 +0700

    U7 RZ

---
+++ controllers/backup.js
@@ -0,0 +1,20 @@
+static addMenu (req,res) {
+    res.render('TAMBAH MENU')
+}
+
+static createMenu (req,res) {
+    const {name, price, category, description} = req.body
+    Menu.create({name, price, category, description})
+        .then((menu) => {
+            console.log(menu);
+            res.redirect(`/MENU UTAMA`)
+        })
+        .catch((err) => { 
+            if (err.name == 'SequelizeValidationError') {
+                const messages = err.errors.map((el) => el.message)
+                res.send(messages)
+            } else {
+                res.send(err)
+            }
+        })
+}
--- controllers/controller.js
+++ controllers/controller.js
@@ -2,6 +2,27 @@ class Controller {
     static home (req,res) {
         res.render('log_in_page')
     }
+
+    static addMenu (req,res) {
+        res.render('TAMBAH MENU')
+    }
+
+    static createMenu (req,res) {
+        const {name, price, category, description} = req.body
+        Menu.create({name, price, category, description})
+            .then((menu) => {
+                console.log(menu);
+                res.redirect(`/MENU UTAMA`)
+            })
+            .catch((err) => { 
+                if (err.name == 'SequelizeValidationError') {
+                    const messages = err.errors.map((el) => el.message)
+                    res.send(messages)
+                } else {
+                    res.send(err)
+                }
+            })
+    }
 }
 
 module.exports = Controller
