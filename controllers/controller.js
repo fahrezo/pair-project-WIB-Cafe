6604e0cca03297cfe2127ee7b323ffe188b1dead
dkytmshrn <dkytmshrn@gmail.com>
2023-09-28 18:54:17 +0700

    U7 TM

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,3 +1,5 @@
+// const {User} = require ('../models')
+
 class Controller {
     static home (req,res) {
         res.render('log_in_page')
@@ -8,7 +10,14 @@ class Controller {
     }
 
     static postRegister (req, res) {
-        res.send(req.body)
+        const {name, email, password, phone, address, role, adminPassword} = req.body
+        console.log(name, email, password, phone, address, role, adminPassword);
+        if (adminPassword==="sapilaras") {
+            res.send(req.body)
+        } else {
+            res.send(`Admin Password Salah`)
+        }
+        // User.create ({name:name, email:email, password:password, phone:phone, address:address})
     }
 }
 
