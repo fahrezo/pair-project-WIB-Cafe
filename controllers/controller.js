a93e5eef3540ef0722fce48999c9c1a31def48ab
dkytmshrn <dkytmshrn@gmail.com>
2023-09-28 17:21:13 +0700

    U6 TM

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,6 +2,14 @@ class Controller {
     static home (req,res) {
         res.render('log_in_page')
     }
+
+    static register (req, res) {
+        res.render('register_page')
+    }
+
+    static postRegister (req, res) {
+        res.send(req.body)
+    }
 }
 
 module.exports = Controller
